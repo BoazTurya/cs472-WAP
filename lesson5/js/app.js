5726b79df82e3d54d5f2f150cbebaa46c7c9fe27
BoazTurya <boazturya@gmail.com>
2021-03-27 14:35:04 -0500

    Update app.js

--- lesson5/js/app.js
+++ lesson5/js/app.js
@@ -1,7 +1,20 @@
 let x = "thsla"
 console.log(x);
 
+let myarray = [];
 var i;
 for(i=0;i<=4;i++){
     console.log(i);
+    myarray.push(
+        function(){
+            return i;
+        }
+    )
+
 }
+ console.log("after i= "+i);
+ console.log(myarray[1]());
+ console.log(myarray[2]());
+ console.log(myarray[3]());
+ console.log(myarray[4]());
+
